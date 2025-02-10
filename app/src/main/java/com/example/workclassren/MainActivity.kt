05e01ffee516530fde9faff9a02fd36a1c1ed3db
renacer <layshargolz@gmail.com>
2025-02-10 07:51:31 -0600

    Use of simple layout components

--- app/src/main/java/com/example/workclassren/MainActivity.kt
+++ app/src/main/java/com/example/workclassren/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -19,8 +22,21 @@ class MainActivity : ComponentActivity() {
       //  enableEdgeToEdge()
         setContent {
             WorkClassRenTheme {
-                Text(text = "Hello World")
-                Text(text = "Welcome to your first application")
+                Row(){
+                    Column(){
+                        Text(text = "Hello World")
+                        Text(text = "Welcome to your first application")
+                    }
+                   Column(){
+                       Text(text = "Hello World")
+                       Text(text = "Welcome to your first application")
+                   }
+                    Column(){
+                        Text(text = "Hello World")
+                        Text(text = "Welcome to your first application")
+                    }
+                }
+
 /*                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
                         name = "Android",
