c31fea8fe5f59dbc080aac58d008b2e32a6b765b
renacer <layshargolz@gmail.com>
2025-02-12 07:53:24 -0600

    Box layout and Text properties

--- app/src/main/java/com/example/workclassren/MainActivity.kt
+++ app/src/main/java/com/example/workclassren/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Scaffold
@@ -21,32 +22,42 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.app.NotificationCompat.Style
 import com.example.workclassren.ui.theme.WorkClassRenTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-      //  enableEdgeToEdge()
+        //  enableEdgeToEdge()
         setContent {
             WorkClassRenTheme {
-                Column(){
-                    Column(){
+                Column() {
+                    Column() {
                         TextComposable("Renatta")
                         TextComposable()
                         TextComposable()
                         TextComposable()
                     }
-                    Row(){
+                    Row() {
                         TextComposable()
                         TextComposable()
                         TextComposable()
                         TextComposable()
                     }
-                    Column(){
+                    Column() {
                         ModifierExample2()
+                        ModifierExample4()
+                        CustomText()
                     }
                 }
 
@@ -56,20 +67,20 @@ class MainActivity : ComponentActivity() {
 
     @Preview(showBackground = true)
     @Composable
-    fun TextComposable (name:String = "Empty"){
-        Text ("Welcome")
+    fun TextComposable(name: String = "Empty") {
+        Text("Welcome")
         Text(name)
     }
 
     @Preview(showBackground = true)
     @Composable
-    fun ModifierExample1(){
-Column(
-    modifier = Modifier
-        .padding(40.dp, 30.dp, 20.dp, 10.dp)
-){
-    Text("Hello World")
-}
+    fun ModifierExample1() {
+        Column(
+            modifier = Modifier
+                .padding(40.dp, 30.dp, 20.dp, 10.dp)
+        ) {
+            Text("Hello World")
+        }
     }
 
     @Preview(showBackground = true)
@@ -79,33 +90,80 @@ Column(
             modifier = Modifier
                 .padding(24.dp)
                 .fillMaxWidth()
-                .clickable(onClick = {clickAction()})
+                .clickable(onClick = { clickAction() })
         ) {
             Text("Hello World")
         }
     }
-    fun clickAction(){
+
+    fun clickAction() {
         println("Column Clicked")
-        }
+    }
 
     @Preview(showBackground = true)
     @Composable
-    fun ModifierExample3(){
-    Column (
-        modifier = Modifier
-            .fillMaxHeight()
-            .padding(16.dp)
-            .background(Color.Red)
-            .border(width = 2.dp, color = Color.Blue)
-            .width(200.dp),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceEvenly
-    ){
-        TextComposable("1")
-        TextComposable("2")
-        TextComposable("3")
-        TextComposable("4")
+    fun ModifierExample3() {
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(16.dp)
+                .background(Color.Green)
+                .border(width = 2.dp, color = Color.Blue)
+                .width(200.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+            TextComposable("1")
+            TextComposable("2")
+            TextComposable("3")
+            TextComposable("4")
+        }
     }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun ModifierExample4() {
+        Box(
+            modifier = Modifier
+                .background(Color.Red)
+                .padding(10.dp)
+                .height(300.dp)
+                .width(300.dp)
+        ) {
+            Text("1", Modifier.align(Alignment.TopStart))
+            Text("2", Modifier.align(Alignment.TopCenter))
+            Text("3", Modifier.align(Alignment.TopEnd))
+            Text("4", Modifier.align(Alignment.CenterStart))
+            Text("5", Modifier.align(Alignment.Center))
+            Text("6", Modifier.align(Alignment.CenterEnd))
+            Text("7", Modifier.align(Alignment.BottomStart))
+            Text("8", Modifier.align(Alignment.BottomCenter))
+            Text("9", Modifier.align(Alignment.BottomEnd))
+        }
     }
-}
+
+    @Preview(showBackground = true)
+    @Composable
+    fun CustomText(){
+        Column(){
+            Text(
+                stringResource(R.string.sample_text),
+                color = colorResource(R.color.purple_200),
+                fontSize = 20.sp,
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.ExtraBold
+            )
+            val gradientColors = listOf(Color.Red, colorResource(R.color.purple_200) , Color.Blue, Color.Green)
+            Text(
+                stringResource(R.string.sample_text),
+                style = TextStyle(brush = Brush.linearGradient(colors = gradientColors))
+            )
+        }
+    }
+
+
+}//close class
+
+
+
 
