74119dc18815ff57805176013d14264a972979be
renacer <layshargolz@gmail.com>
2025-03-09 11:56:23 -0600

    Added the code of the third video: Jetpack Compose Components Progress Indicators

--- app/src/main/java/com/example/workclassren/ui/screens/ComponentsScreen.kt
+++ app/src/main/java/com/example/workclassren/ui/screens/ComponentsScreen.kt
@@ -3,15 +3,19 @@ package com.example.workclassren.ui.screens
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LargeFloatingActionButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.material3.Text
@@ -20,12 +24,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 
 @Composable
 fun ComponentsScreen(navController: NavHostController){
        // Buttons()
-    FloatingButtons()
+        //FloatingButtons()
+    Progress()
 }
 
 //@Preview(showBackground = true)
@@ -78,5 +84,25 @@ fun FloatingButtons() {
             Icon(Icons.Filled.Add, "Add Button")
             Text("Button")
         }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun Progress() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        LinearProgressIndicator(
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+        CircularProgressIndicator(
+            modifier = Modifier
+                .width(64.dp)
+        )
     }
 }
